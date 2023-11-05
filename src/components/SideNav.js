7c753663401069ebeeebb35980c52a870c0e2269
ToluwanimiO <toluwanimiosuolale@gmail.com>
2023-11-05 12:50:34 +0100

    profile page

---
+++ src/components/SideNav.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styles from '@/styles/Home.module.css'
+import Image from 'next/image'
+import Link from 'next/link';
+import { usePathname } from "next/navigation";
+
+const navItems = ['Profile','Appointments','Notifications']
+
+const Header = () => {
+  const pathname = usePathname();
+
+  return (
+    <header className={styles.sideBarUser}>
+    <Image
+        className={styles.sideNavLogo}
+        src="/ui-logo.svg"
+        alt="UI Logo"
+        width={120}
+        height={37}
+        priority
+      />
+      {navItems.map((item)=>(
+        <Link href={`/client/${item.toLowerCase()}`} className={`${pathname == `/client/${item.toLowerCase()}` ? styles.activeNav : ""} ${styles.iconDiv}`}>
+        <Image
+          className={styles.icon}
+          src={`/${item}.svg`}
+          alt={item}
+          width={120}
+          height={37}
+          priority
+        />
+        <p>{item}</p>
+        </Link>
+      ))}
+      <div className={styles.iconDiv}>
+        <Image
+          className={styles.icon}
+          src="/Logout.svg"
+          alt="Logout"
+          width={120}
+          height={37}
+          priority
+        />
+        <p>Logout</p>
+        </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/client/login.js
+++ src/pages/client/login.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -41,7 +42,9 @@ export default function Home() {
                 <input />
             </div>
             <p className={`${styles.blueText} ${styles.alignCenter}`}>Forgot your password?</p>
-            <button className={styles.btnForm}>Login</button>
+            <button className={styles.btnForm}>
+              <Link href="/client/profile">Login</Link>
+            </button>
             <span className={styles.bold}>Don't have an account?</span><span className={styles.blueText}> Create one here</span>
         </form>
         </div>
